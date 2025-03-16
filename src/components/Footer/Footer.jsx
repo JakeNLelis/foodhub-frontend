76e5ebd00172f175125308cb5b3d020bae47ce47
JakeNLelis <jakelelis05@gmail.com>
2025-03-16 00:21:32 -0800

    [Web] Implement Footer design
    Implemented the footer design.
    Minimal adjustment in the z-index of the dropdown
    menu in the navigation bar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 function Footer() {
   return (
     <div>
-      Footer
+      <div className="footer">
+        <div className="footer-top">
+          <h2>Need update on Latest Offers?</h2>
+          <p>Subscribe on our newsletter to get frequent update</p>
+          <div className="input-footer">
+            <input type="email" placeholder="Enter your email" />
+            <button>Subscribe</button>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="footer-left">
+            <h2>FoodHub</h2>
+            <div className="socials">
+              <a href="#">
+                <FaFacebook className="social-icon" />
+              </a>
+              <a href="#">
+                <FaInstagram className="social-icon" />
+              </a>
+              <a href="#">
+                <FaYoutube className="social-icon" />
+              </a>
+            </div>
+          </div>
+          <div className="footer-right">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <p className="copy">© 2025 FoodHub. All rights reserved.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
